e0a65a407539922001567ced900755437184f579
cheymonty <45781686+cheymonty@users.noreply.github.com>
2020-01-04 14:10:14 -0500

    This my first version of the CodePath app

--- CPSubmission/ViewController.swift
+++ CPSubmission/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    var backgroundColor: UIColor!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        backgroundColor = view.backgroundColor
     }
 
-
+    @IBAction func didTappedButton(_ sender: Any) {
+        print("Hello")
+        textLabel.textColor = UIColor.orange
+    }
+    
+    @IBAction func didTapBackgroundButton(_ sender: Any) {
+        view.backgroundColor = UIColor.blue
+    }
+    
+    @IBAction func didTapTextButton(_ sender: Any) {
+       // textLabel.text = "Goodbye!"
+        textLabel.text = textField.text
+        textField.text = ""
+        view.endEditing(true)
+    }
+    
+    @IBAction func onResetGesture(_ sender: Any) {
+        textLabel.text = "Hello from Cheyenne"
+        view.backgroundColor = backgroundColor
+        textLabel.textColor = UIColor.black
+        
+    }
 }
 
